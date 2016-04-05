ffa22a17a8510026b7a78eb0423ebcd15400d378
robin <robgro@telia.com>
2016-04-05 14:07:11 +0200

    Implemented heapsort

--- main.cpp
+++ main.cpp
@@ -3,40 +3,44 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-  std::vector<std::vector<int>> v;
-  std::sort(nums.begin(), nums.end());
-
-  int n = nums.size();
-  int last_v1 = INT_MIN;
-
-  for (int i = 0; i < n - 2; ++i){
-    int v1 = nums[i];
-    if(v1 == last_v1) continue;
-    last_v1 = v1;
-    int startindex = i + 1;
-    int endindex = n - 1;
-    while(startindex < endindex){
-      int v2 = nums[startindex];
-      int v3 = nums[endindex];
-      int sum = v1 + v2 + v3;
-      if(sum == 0){
-        v.push_back( {v1,v2, v3});
-        std::cout << "{" << v1 << "," << v2 << "," << v3 << "}" << std::endl;
-        while(nums[endindex] == v3 && endindex > startindex) --endindex;
-      } else if(sum > 0){
-        while(nums[endindex] == v3 && endindex > startindex) --endindex;
-      } else{
-        while(nums[startindex] == v2 && endindex > startindex) ++startindex;
+auto myheapsort = [](auto begin, auto end) {
+  int n = end - begin;
+  //Build heap
+  struct {
+    int* begin, * end;
+    void stableize(int i){  //log n time
+      int rightindex = (i + 1) * 2;
+      int leftindex = rightindex - 1;
+      if(begin + leftindex < end){
+        int* v = begin;
+        //if there is only one child take that, otherwise take biggest
+        int childindex = (v[leftindex] > v[rightindex] || begin + rightindex == end)? leftindex : rightindex;
+        int& parent = v[i];
+        int& child = v[childindex];
+        if(child > parent){
+          std::swap(child, parent);
+          // we have to recursively stabilize child in case we swapped
+          stableize(childindex);
+        }
       }
     }
+  } recursive{&*begin,&*end};
+  for (int i = (n - 1) / 2; i >= 0; --i){ 
+    recursive.stableize(i);
   }
-  return v;
-}
+
+  //sort the heap
+  while(recursive.end - recursive.begin > 0){
+    std::swap(*recursive.begin,*(recursive.end - 1));
+    --recursive.end;
+    recursive.stableize(0);
+  }  
+};
 
 int main(int argc, char ** args){
-  std::vector<int> v = {-1, 0, 1, 2, -1, -4};
-  //std::vector<int> v = {8,-15,-2,-13,8,5,6,-3,-9,3,6,-6,8,14,-9,-8,-9,-6,-14,5,-7,3,-10,-14,-12,-11,12,-15,-1,12,8,-8,12,13,-13,-3,-5,0,10,2,-11,-7,3,4,-8,9,3,-10,11,5,10,11,-7,7,12,-12,3,1,11,9,-9,-4,9,-12,-6,11,-7,4,-4,-12,13,-8,-12,2,3,-13,-12,-8,14,14,12,9,10,12,-6,-1,8,4,8,4,-1,14,-15,-7,9,-14,11,9,5,14};
-  threeSum(v);
+  std::vector<int> v = {1,10,2,9,6,4,2,34,5,6,75,3,5,6,7,7};
+  myheapsort(v.begin(), v.end());
+  for (auto && i : v) std::cout << i << " ";
+  std::cout << std::endl;
   return 0;
 }
