1df8f92caed6b682c8433ec300bdac65024f4e9f
robin <robgro@telia.com>
2016-04-13 22:50:59 +0200

    Solution now has a time complexity O(nlogn) and mem complexity of O(n)

--- main.cpp
+++ main.cpp
@@ -2,15 +2,6 @@
 #include <vector>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-  for(int i = 0; i < nums.size(); ++i){
-    for(int j = i + 1; j < nums.size(); ++j){
-      if(nums[i] + nums[j] == target ) return vector<int>{i,j};
-    }
-  }
-  return vector<int>{-1,-1};
-}
-
 template<typename T>
 std::ostream & operator<<(std::ostream & os, std::vector<T> vec){
     os << "{ ";
@@ -19,8 +10,41 @@ std::ostream & operator<<(std::ostream & os, std::vector<T> vec){
     return os;
 }
 
+vector<int> twoSum(vector<int>& nums, int target) {
+  vector<int> nums_sorted = nums;
+  sort(nums_sorted.begin(), nums_sorted.end());
+  int start_index = 0;
+  int end_index = nums_sorted.size() - 1;
+  while(start_index < end_index){
+    int sum = nums_sorted[start_index] + nums_sorted[end_index];
+    if(sum > target) end_index--;
+    else if(sum < target) start_index++;
+    else {
+      //find indicies
+      int i = 0;
+      int last_target = -1;
+      for (;i < nums_sorted.size(); ++i) {
+        if(nums[i] == nums_sorted[start_index]) {
+          last_target = end_index;
+          break;
+        }
+        if(nums[i] == nums_sorted[end_index]){
+          last_target = start_index;
+          break;
+        }
+      }
+      int j = i + 1;
+      for (;j < nums_sorted.size(); ++j){
+        if(nums[j] == nums_sorted[last_target]) break;
+      } 
+      return vector<int>{i,j};
+    }
+  }
+  return vector<int>{-1,-1};
+}
+
 int main(int argc, char ** args){
-  std::vector<int> v = {-1, 0, 1, 2, -1, -4};
-  std::cout << twoSum(v, -3) << std::endl;
+  std::vector<int> v = {-1,-2,-3,-4,-5};
+  std::cout << twoSum(v, -8) << std::endl;
   return 0;
 }
