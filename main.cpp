d5c2a03c605d4abdbe5694fbb19f22f10408e4d5
robin <robgro@telia.com>
2016-04-13 22:09:36 +0200

    C++ solution looks more like c++

--- main.cpp
+++ main.cpp
@@ -2,29 +2,32 @@
 using namespace std;
 
 int lengthOfLongestSubstring(string s) {
-  char* str = (char*)s.c_str();
   int sol = 0;
+  int start_index = 0;
+  int end_index = 0;
   bool used_characters[128];
-  char* start_p = str;
-  char* end_p = str;
-  for(bool* i = used_characters; i < used_characters + 128; ++i) *i = 0;
-  while(*end_p != '\0'){
-    char current_char = *end_p;
+  for(int i = 0; i < 128; ++i){
+      used_characters[i] = 0;
+  }
+  while(end_index < s.length()){
+    char current_char = s[end_index];
+    //is current character used?
     if(used_characters[current_char]){
-      int current_sol = end_p - start_p;
       //check length if current solution is a better solution
+      int current_sol = end_index - start_index;
       if(current_sol > sol) sol = current_sol;
       // search for repeated character, and remove every in the way
-      while(*start_p != current_char){
-        used_characters[*start_p] = 0;
-        ++start_p;
+      while(s[start_index] != current_char){
+        used_characters[s[start_index]] = 0;
+        ++start_index;
       } 
       // move away from that character, it is still in use, so dont reset it
-      ++start_p;
-    } else used_characters[current_char] = 1; 
-    ++end_p;
+      ++start_index;
+
+    } else used_characters[current_char] = 1;
+    ++end_index;
   }
-  return max(sol, (int)(end_p - start_p));
+  return max(sol, end_index - start_index);
 } 
 
 int main(int argc, char ** args){
