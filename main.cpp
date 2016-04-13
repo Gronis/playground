110bda755a69046bef93e8e6d1364225dbd5da0d
robin <robgro@telia.com>
2016-04-13 22:57:08 +0200

    Reset the master branch so that new solutions can derive it without removing a bunch of code

--- main.cpp
+++ main.cpp
@@ -3,12 +3,5 @@
 #include <sstream>
 
 int main(int argc, char ** args){
-  std::string token, s = "hej,jag,gillar,kakor";
-  std::stringstream ss(s);
-
-  std::string ar[4];
-  for(int i = 0; i < 4 && getline(ss,token,','); ++i) ar[i] = token;
-  reverse(&ar[0], &ar[4]);
-  for(int i = 0; i < 4; ++i) std::cout << ar[i] << " ";
-  std::cout << std::endl;  
+  std::cout << "Hello World" << std::endl; 
 }
