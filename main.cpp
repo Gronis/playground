5877502c4efbb238f669703c32294258877792e5
robin <robgro@telia.com>
2016-04-17 22:24:20 +0200

    Switched to an inline solution

--- main.cpp
+++ main.cpp
@@ -2,13 +2,32 @@
 #include <string>
 #include <sstream>
 
-int main(int argc, char ** args){
-  std::string token, s = "hej,jag,gillar,kakor";
-  std::stringstream ss(s);
+void reverse(std::string::iterator begin, std::string::iterator end){
+  end--;
+  while(begin < end){
+    char tmp = *begin;
+    *begin = *end;
+    *end = tmp;
+    begin++; end--;
+  }
+}
 
-  std::string ar[4];
-  for(int i = 0; i < 4 && getline(ss,token,','); ++i) ar[i] = token;
-  reverse(&ar[0], &ar[4]);
-  for(int i = 0; i < 4; ++i) std::cout << ar[i] << " ";
-  std::cout << std::endl;  
+void reverse_words(std::string& s){
+  auto start = s.begin();
+  for(auto it = s.begin(); it < s.end(); ++it){
+    if(*it == ' '){
+      reverse(start, it);
+      start = it + 1;
+    }
+  }
+  if(start < s.end()) reverse(start, s.end());
+  reverse(s.begin(), s.end());
+}
+
+int main(int argc, char ** args){
+  std::string token, s = "hej jag gillar kakor";
+ 
+  std::cout << s << std::endl;
+  reverse_words(s);
+  std::cout << s << std::endl;
 }
