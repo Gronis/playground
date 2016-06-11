3befade9fd1f2dbc86942f1e4ff346786bf45719
robin <robgro@telia.com>
2016-06-11 13:27:03 +0200

    more readable code. Added help function and comments

--- main.c
+++ main.c
@@ -8,17 +8,34 @@ typedef struct node_t {
 
 typedef node* list;
 
-void remove_from_list(list* l, int value){
+// returns a pointer of list to the next element
+list* next(list* l){
+  return &(*l)->next;
+}
+
+// returns true if no next more elements exists. false otherwise
+int empty(list* l){
+  return *l == NULL;
+}
+
+// get the value of the first element in the list
+int value(list* l){
+  return (*l)->value;
+}
+
+// remove a specific element from the list. O(n) time complexity
+void remove_from_list(list* l, int v){
   if(*l == NULL) return;
-  node** it = l;
-  while((*it)->next && (*it)->value != value) it = &(*it)->next;
-  if((*it)->value == value) {
+  list* it = l;
+  while(!empty(next(it)) && value(it) != v) it = next(it);
+  if(value(it) == v) {
     node* deleted_node = *it;
     *it = deleted_node->next;
     free(deleted_node);
   }
 }
 
+// add a new element to the list at the front. O(1) time complexity
 void add_to_list(list* l, int value){
   node* n = (node*)malloc(sizeof(node));
   n->next = (*l);
@@ -26,16 +43,41 @@ void add_to_list(list* l, int value){
   (*l) = n;
 }
 
+// allocates memory to a new list
 list* new_list(){
   list* l = (list*)malloc(sizeof(list));
   *l = NULL;
   return l;
 }
 
+// delete the list and its elements
+void delete_list(list* l){
+  list* head = l;
+  // remove first element until it is empty
+  while(!empty(l)){
+    remove_from_list(l, value(l));
+  }
+  free(head);
+}
+
+// print each element in the list with a space separating each element
+void print_list(list* l){
+  list* it = l;
+  while(!empty(it)){
+    printf("%d ", value(it));
+    it = next(it);
+  }
+  printf("\n");
+}
+
+
 int main(){
   list* l = new_list();
+
   // 0 -> 1 -> 2 -> 3 -> NULL
-  for(int i = 3; i >= 0; --i) add_to_list(l,i);
+  for(int i = 3; i >= 0; --i) {
+    add_to_list(l,i);
+  }
 
   // 1 -> 2 -> NULL
   remove_from_list(l, 3);
@@ -44,12 +86,10 @@ int main(){
   // 1 -> 2 -> NULL
   remove_from_list(l, 7);
 
-  // print the list (and remove elements)
-  while(*l != NULL){
-    printf("%d\n", (*l)->value);
-    remove_from_list(l, (*l)->value);
-  }
+  // 5 -> 1 -> 2 -> NULL
+  add_to_list(l, 5);
 
-  free(l);
+  print_list(l);
+  delete_list(l);
   return 0;
 }
