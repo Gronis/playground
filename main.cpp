7d84bcc21c7bc7ca0e7d15de728eb89b8db0e051
robin <robgro@telia.com>
2016-06-18 17:14:51 +0200

    Updated solution

--- main.cpp
+++ main.cpp
@@ -5,19 +5,22 @@
 int main(int argc, char ** args){
   if(argc < 2){
     std::cout << "Must provide an number" << std::endl;
+    return 0;
   }
-  int n = std::stoi(args[1]);
+  int num = std::stoi(args[1]);
 
   std::vector<int> ar;
-  ar.resize(n);
-
+  ar.resize(num + 1);
   //iterative since mem complexity O(n)
-  for (int i = 1; i <= n; ++i){
-    ar[i] = (i & 1) == 1;
-    if(i >> 1 > 0) ar[i] += ar[i >> 1];
+  for (int i = 1; i <= num; ++i){
+      ar[i] = (i & 1) == 1;
+      int iover2 = i >> 1;
+      if(iover2 > 0) ar[i] += ar[iover2];
   }
 
-  for (int i = 0; i <= n; ++i){
+  for (int i = 0; i <= num; ++i){
     std::cout << ar[i] << " ";
   }
+  std::cout << std::endl;
+  return 0;
 }
