3fbfcd898bd7e1a2b11ec63753cf01c93f3aaeff
robin <robgro@telia.com>
2016-06-19 16:14:16 +0200

    first kind of working solution

--- main.cpp
+++ main.cpp
@@ -2,59 +2,70 @@
 #include <vector>
 #include <cmath>
 
-std::size_t bin_search(std::vector<int>& v, int val){
-  auto lower = 0UL, upper = v.size() - 1;
-  while(lower < upper){
-    auto half = (upper - lower) / 2;
-    if(half < 2) half = 1;
-    if(v[lower + half] > val) upper -= half;
-    else                      lower += half;
-  }
-  if(lower == 0            && v[lower] > val) return -1;
-  //if(lower == v.size() - 1 && v[lower] < val) return v.size();
-  return lower;
-}
 
-double findMedian_inner(std::vector<int>& v1, std::vector<int>& v2) {
-  int i1 = 0;
-  int i2 = 0;
-  int size_both = v1.size() + v2.size();
-  int half_size_both = size_both / 2;
-  int steps = v1.size();
-  int lower_bound_size = 0;
-  do{
-    lower_bound_size = i1 + 1 + i2 + 1;
-    steps = steps > 1? steps / 2 : 1;
-    if(lower_bound_size > half_size_both) i1 -= steps;
-    else                                  i1 += steps;
-    i2 = bin_search(v2, v1[i1]);
-    std::cout << "i1: " << i1 << ", i2: " << i2 << std::endl;
-  } while(abs(lower_bound_size - half_size_both) > 1);
-
-  // extract median
-  int highest_low = i2 == -1 || v1[i1] > v2[i2]? v1[i1] : v2[i2];
-  i2++; i1++;
-  int lowest_high = i2 > v2.size() || v1[i1] < v2[i2]? v1[i1] : v2[i2];
-  if(lower_bound_size * 2 > size_both){
-    return highest_low;
-  }
-  else if(lower_bound_size  * 2 < size_both){
-    return lowest_high;
-  }
-  else{
-    return (lowest_high + highest_low) / 2;
-  }
-}
 
 double findMedianSortedArrays(std::vector<int>& v1,
-                              std::vector<int>& v2) {
-  return v1.size() >= v2.size()? findMedian_inner(v1, v2) :
-                                 findMedian_inner(v2, v1);
+  /* v1 is biggest */         std::vector<int>& v2) {
+  if(v1.size() < v2.size()) std::swap(v1,v2);
+
+  //setup itervals
+  struct interval {
+    std::vector<int>& v;
+    std::size_t begin;
+    std::size_t end;
+    std::size_t i;
+  }
+  iv1{v1, 0,v1.size() - 1, v1.size() / 2 - 1},
+  iv2{v2, 0,v2.size() - 1, v2.size() / 2 - 1};
+
+  // setup helper functions
+  auto expand_set = [&]() -> interval &{
+    if(v1[iv1.i + 1] < v2[iv2.i + 1]) return iv1;
+    else return iv2;
+  };
+  auto reduce_set = [&]() -> interval &{
+    if(v1[iv1.i] > v2[iv2.i]) return iv1;
+    else return iv2;
+  };
+  auto expand = [&](){
+    auto& iv = expand_set();
+    auto steps = (iv.end - iv.i) / 2;
+    if(steps < 1) steps = 1;
+    iv.begin = iv.i + 1;
+    iv.i = iv.begin + steps;
+  };
+  auto reduce = [&](){
+    auto& iv = reduce_set();
+    auto steps = (iv.i - iv.begin) / 2;
+    if(steps < 1) steps = 1;
+    iv.end = iv.i;
+    iv.i = iv.end - steps;
+  };
+  int const total_size = v1.size() + v2.size();
+  auto small_set_size = [&](){ return (int)(iv1.i + iv2.i + 2); };
+  auto large_set_size = [&](){ return total_size - small_set_size(); };
+  auto largest_min    = [&](){ return std::max(v1[iv1.i], v2[iv2.i]); };
+  auto smallest_max   = [&](){ return std::min(v1[iv1.i + 1], v2[iv2.i + 1]);};
+  auto unbalanced = [&](){ return abs(total_size - small_set_size() * 2) > 1;};
+
+  // main algorithm
+  while (unbalanced() || largest_min() > smallest_max()){
+    if(small_set_size() > large_set_size())  {
+      reduce();
+    } else {
+      expand();
+    }
+    std::cout << "[" << iv1.begin << " i1: " << iv1.i << " ]" << iv1.end << std::endl;
+    std::cout << "[" << iv2.begin << " i2: " << iv2.i << " ]" << iv2.end << std::endl;
+    std::cout << "smallest max: " << smallest_max() << std::endl;
+    std::cout << "largest  min: " << largest_min()  << std::endl;
+  }
+  return 0;
 }
 
 int main(int argc, char ** args){
-  std::vector<int> v1 = {0,5,6,7,8,9};
-  std::vector<int> v2 = {0,1,2,3,4};
+  std::vector<int> v1 = {0,5,6,7,8,9,10,11,12};
+  std::vector<int> v2 = {0,1,2,3,4,5,6,7,8};
   std::cout << findMedianSortedArrays(v1,v2) << std::endl;
   return 0;
 }
